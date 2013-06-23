2914755a4b6a070735f86438fce8db4900c931ea
Michael Thorpe <mt2309@ic.ac.uk>
2013-06-23 13:44:51 +0100

    Update

--- src/main/c/build.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/local/bin/bash
-
-clang -std=c11 -Weverything -Wno-padded -Wno-unused-parameter -Wno-unused-label -Wno-unused-variable -g pony_class_ids.c pony_class.c
--- test/typechecker/typeclass.pony
+++ test/typechecker/typeclass.pony
@@ -1,4 +1,4 @@
-trait Imm[Int] {
+trait Imm[K] {
 
 }
 
